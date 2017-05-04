558586cfa99090e0002218d5c2105fe4d3ac8279
Vladislav Legoshin <abbatchi@gmail.com>
2017-05-04 10:25:05 +0200

    fibonacci add

--- index.php
+++ index.php
@@ -2,5 +2,23 @@
 
 	<img src="img/drac.jpg" class="superbg" />
 
+	<?php
+	  if ($_SESSION) {
+	  	echo "<h1>FIBONACCI IT!</h1>";
+			$a = array();
+			$a[1] = '1';
+			$a[2] = '1';
+				for ($i = 3; $i < 200; $i++) {
+				  $a[$i] = bcadd($a[$i-1], $a[$i-2]);
+				  }
+
+				for ($i = 1; $i < 200; $i++) {
+				  echo($i."\t - ".$a[$i].", <br/>\n");
+				  }
+			echo("...\n");
+			}
+
+	?>
+
 
 <?php require 'footer.php';?>
